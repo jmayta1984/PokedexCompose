0e6cdcb72f7c8630c2271c6e9f1ca7c8527620b3
Jorge Mayta <jmayta@pucp.edu.pe>
2022-09-20 13:51:09 -0500

    initial commit

--- app/src/main/java/pe/edu/upc/pokedexcompose/data/models/Pokemon.kt
+++ app/src/main/java/pe/edu/upc/pokedexcompose/data/models/Pokemon.kt
@@ -1,5 +1,13 @@
 package pe.edu.upc.pokedexcompose.data.models
 
-class Pokemon (val name: String, val url:String, val height: Int, val weight: Int){
+class Pokemon (
+
+    val name: String,
+
+    val url:String,
+
+    val height: Int,
+
+    val weight: Int){
     constructor(): this("","",1,1)
 }
